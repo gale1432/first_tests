817aa0bf29eb0f23e18f6817f6e1239496dd8b5e
gale1432 <glars1432@gmail.com>
2025-04-12 20:05:41 -0600

    Added EWT method

--- first_test.py
+++ first_test.py
@@ -3,16 +3,16 @@ import mne
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from pathlib import Path as pt
 import joblib
 import scipy as sp
 import pywt
 import os
 from montage import NewMontage
 from feat_creation import get_stats
+import ewtpy
 #from fathon import DFA
 
-from mne.conftest import event_id
+#from mne.conftest import event_id
 from sklearn.preprocessing import LabelEncoder
 
 #mne.viz.set_browser_backend("qt")
@@ -31,37 +31,38 @@ EVENT_DICT = {
     'nesz': 11,
     'bckg': 12
 }
-CED_PATH = os.path.join(os.path.expanduser('~'), 'Documents', 'tuh_eeg', 'edf')
+CED_PATH = os.path.join(os.path.expanduser('~'), 'sda1', 'Documents', 'tuh_eeg', 'edf')
 HOME_PATH='E:\\Files\\tuh_eeg\\edf\\train'
 
 #train_file = glob(CED_PATH+'\\train\\**/*.edf', recursive=True) #for windows
-train_file = glob(CED_PATH+'/train/**/*.edf', recursive=True) #for linux
-print(len(train_file))
+
 
 def read_data(file_path, preprocessing_type):
+    #mne.datasets.sample.data_path(force_update=True, download=True)
     data = mne.io.read_raw_edf(file_path, preload=True)
     data.set_eeg_reference('average')
     data = data.resample(250.0)
-    #data = data.filter(52, 48)
+
+    data = data.notch_filter(60.0)
     annotations = read_annotation(file_path)
     annotations_obj = create_annotations(annotations)
     data.set_annotations(annotations_obj)
-    events, event_id = mne.events_from_annotations(raw=data, event_id=EVENT_DICT)
-    print(events.shape)
-    print(events)
+    events, ev_id = mne.events_from_annotations(raw=data, event_id=EVENT_DICT)
+    #print(events.shape)
+    #print(events)
     try:
-        epochs = mne.make_fixed_length_epochs(raw=data, duration=1.8)
+        epochs = mne.make_fixed_length_epochs(raw=data, duration=2)
         epochs.events = events
-        epochs.event_id = event_id
+        epochs.event_id = ev_id
         epochs.selection = np.arange(len(events))
-        epochs.drop_log = tuple(() if k in epochs.selection else ("IGNORED",) for k in range(600))
+        epochs.drop_log = tuple(() if k in epochs.selection else ("IGNORED",) for k in range(6000))
         """print(len(epochs.drop_log))
         print(len(epochs.selection))"""
-        print(epochs.drop_log)
+        #print(epochs.drop_log)
         epoch_dataframe = epochs.to_data_frame()
         new_montage = NewMontage(epoch_dataframe)
         epoch_dataframe = new_montage.change_montage()
-        print(epoch_dataframe)
+        #print(epoch_dataframe)
     except ValueError:
         print("This file could not be used!")
         raise ValueError
@@ -90,6 +91,15 @@ def read_data(file_path, preprocessing_type):
         else:
             wpd_columns = [i for i in range(np.shape(wpd_df)[0])]
         return wpd_df, wpd_labels, wpd_columns
+    #********************* THIS IS EMPIRICAL WAVELET TRANSFORM ***********************
+    elif preprocessing_type == 4:
+        #empirical_wt(epoch_dataframe)
+        ewt_df, ewt_labels = empirical_wt(epoch_dataframe)
+        if np.shape(ewt_df)[1]:
+            dwt_columns = [i for i in range(np.shape(ewt_df)[1])]
+        else:
+            dwt_columns = [i for i in range(np.shape(ewt_df)[0])]
+        return ewt_df, ewt_labels, dwt_columns
     #data.plot(duration=20, n_channels=31, bgcolor='white', scalings='auto')
     #print(events)
 
@@ -189,7 +199,7 @@ def discrete_wt(dataframe):
         labels.append(epoch['condition'].unique())
         add_arr = []
         for item in column_names:
-            coefficients = pywt.wavedec(epoch[item].values, wavelet='db4', level=5)
+            coefficients = pywt.wavedec(epoch[item].values, wavelet='db4', level=4)
             #print(type(coefficients))
             #print(len(coefficients))
             for co in coefficients:
@@ -198,6 +208,29 @@ def discrete_wt(dataframe):
             #print(add_arr)
         final_matrix.append(add_arr)
     #print(final_matrix)
+    return pd.DataFrame(final_matrix), pd.Series(labels)
+################################################
+
+def empirical_wt(dataframe):
+    mode_number = 4
+    epoch_number = dataframe['epoch'].max()
+    column_names = get_column_names(dataframe)
+    labels = []
+    final_matrix = []
+
+    for i in range(epoch_number):
+        epoch = dataframe[dataframe['epoch'] == i]
+        labels.append(epoch['condition'].unique())
+        add_arr = []
+        for item in column_names:
+            coefficients, mfb, boundaries = ewtpy.EWT1D(epoch[item].values, N=mode_number)
+            """plt.plot(coefficients)
+            plt.show()"""
+            coefficients_dataframe = pd.DataFrame(coefficients)
+            for i in range(mode_number):
+                stats = get_stats(list(coefficients_dataframe[i]))
+                add_arr.extend(stats)
+        final_matrix.append(add_arr)
     return pd.DataFrame(final_matrix), pd.Series(labels)
 
 def wavelet_packet_decomp(dataframe):
@@ -228,6 +261,7 @@ def process_data(file_path_list, preprocessing_type):
     final_df = pd.DataFrame()
     final_labels = pd.Series()
     file_num = 1
+    unusable_files_counter = 0
     for file_name in file_path_list:
         print("************************************************************")
         print("File number: " + str(file_num))
@@ -236,20 +270,34 @@ def process_data(file_path_list, preprocessing_type):
             final_df = pd.concat([final_df, file_df], axis=0)
             final_labels = pd.concat([final_labels, fft_labels], axis=0)
         except ValueError:
+            unusable_files_counter += 1
             continue
         file_num += 1
-
+    print("Unusable files: " + str(unusable_files_counter))
     return final_df, final_labels
 
 #print(pywt.wavelist(family='db',kind='discrete'))
-df, labels, columns = read_data(train_file[0], 2)
-print(df)
+#df, labels, columns = read_data(train_file[0], 2)
+#print(df)
 #df1, labels1, columns1 = read_data(train_file[-1], 1)
 #print(train_file)
 #print(df)
 #df, labels = process_data(train_file, 1)
-"""df, labels, columns = read_data('/home/gael/Documents/tuh_eeg/edf/train/aaaaaaac/s001_2002/02_tcp_le/aaaaaaac_s001_t000.edf', 3)
-print(df)"""
+def process_all_data(preprocessing_type):
+    train_file = glob(CED_PATH+'/train/**/*.edf', recursive=True) #for linux
+    dev_file = glob(CED_PATH+'/dev/**/*.edf', recursive=True)
+    eval_file = glob(CED_PATH+'/eval/**/*.edf', recursive=True)
+    df, labels = process_data(train_file, preprocessing_type)
+    joblib.dump((df, labels), 'train_ewt_two.sav')
+    df, labels = process_data(dev_file, preprocessing_type)
+    joblib.dump((df, labels), 'dev_ewt_two.sav')
+    df, labels = process_data(eval_file, preprocessing_type)
+    joblib.dump((df, labels), 'eval_ewt_two.sav')
+
+process_all_data(4)
+#df, labels, columns = read_data('/home/gaelh/sda1/Documents/tuh_eeg/edf/train/aaaaaaac/s001_2002/02_tcp_le/aaaaaaac_s001_t000.edf', 2)
+#print(df)
+#read_data('/home/gaelh/sda1/Documents/tuh_eeg/edf/train/aaaaaaac/s001_2002/02_tcp_le/aaaaaaac_s001_t000.edf', 4)
 #print(type(df[0]))
 #print(df[0].apply(type))
 #print(df)
